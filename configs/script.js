0bed1151583ca13e2329194f04ca94ec5eeaef83
CPUcademy <cpucademy@gmail.com>
2024-10-22 19:01:41 +0200

    stable - various changes

--- configs/script.js
+++ configs/script.js
@@ -1,3 +1,64 @@
+const englishText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit nulla ullamcorper ante mollis, ac sagittis metus condimentum. Sed ex justo, mattis vel nulla a, euismod eleifend elit. Donec pellentesque velit ligula, at rutrum metus bibendum sit amet. Nullam lacus quam, aliquet et venenatis at, finibus sed dolor. Duis varius sed ipsum luctus sodales. Donec et odio vestibulum, lobortis risus in, rhoncus lectus. Quisque eu sodales sapien, et consequat ex.";
+const polishText = "Aliquam dictum ipsum in dui convallis hendrerit. Proin vitae egestas lectus, sed efficitur justo. Fusce et auctor nunc. Fusce dictum molestie auctor. In hac habitasse platea dictumst. Fusce ac mi ut dui tincidunt consequat accumsan non justo. Cras non tempus nisi. Ut laoreet tempus elit, sed ultricies neque dictum eget. Sed mollis convallis ipsum, porttitor sollicitudin nibh vulputate non. Nam ultricies tincidunt dolor, a finibus enim.";
+const englishTextE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit nulla ullamcorper ante mollis, ac sagittis metus condimentum. Sed ex justo, mattis vel nulla a, euismod eleifend elit. Donec pellentesque velit ligula, at rutrum metus bibendum sit amet. Nullam lacus quam, aliquet et venenatis at, finibus sed dolor. Duis varius sed ipsum luctus sodales. Donec et odio vestibulum, lobortis risus in, rhoncus lectus. Quisque eu sodales sapien, et consequat ex. Fusce feugiat sapien ligula, sit amet varius tortor aliquet sed. Donec rhoncus ante nec nibh fringilla sodales. Aliquam dictum ipsum in dui convallis hendrerit. Proin vitae egestas lectus, sed efficitur justo. Fusce et auctor nunc. Fusce dictum molestie auctor. In hac habitasse platea dictumst. Fusce ac mi ut dui tincidunt consequat accumsan non justo. Cras non tempus nisi. Ut laoreet tempus elit, sed ultricies neque dictum eget. Sed mollis convallis ipsum, porttitor sollicitudin nibh vulputate non. Nam ultricies tincidunt dolor, a finibus enim. Vivamus consequat urna at magna elementum, vel vulputate turpis laoreet.";
+const polishTextE = "Aliquam dictum ipsum in dui convallis hendrerit. Proin vitae egestas lectus, sed efficitur justo. Fusce et auctor nunc. Fusce dictum molestie auctor. In hac habitasse platea dictumst. Fusce ac mi ut dui tincidunt consequat accumsan non justo. Cras non tempus nisi. Ut laoreet tempus elit, sed ultricies neque dictum eget. Sed mollis convallis ipsum, porttitor sollicitudin nibh vulputate non. Nam ultricies tincidunt dolor, a finibus enim. Vivamus consequat urna at magna elementum, vel vulputate turpis laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit nulla ullamcorper ante mollis, ac sagittis metus condimentum. Sed ex justo, mattis vel nulla a, euismod eleifend elit. Donec pellentesque velit ligula, at rutrum metus bibendum sit amet. Nullam lacus quam, aliquet et venenatis at, finibus sed dolor. Duis varius sed ipsum luctus sodales. Donec et odio vestibulum, lobortis risus in, rhoncus lectus. Quisque eu sodales sapien, et consequat ex. Fusce feugiat sapien ligula, sit amet varius tortor aliquet sed. Donec rhoncus ante nec nibh fringilla sodales.";
+
+const englishFlag = "files/englishFlag.png";
+const polishFlag = "files/polishFlag.png";
+textElement = document.getElementById('cv');
+if(window.location.pathname.endsWith('cv.html'))
+{
+  textElement.innerHTML = englishTextE;
+}
+else
+{
+  textElement.innerHTML = englishText;
+}
+
+function language(additional) 
+{
+    if(additional)
+    {
+      text1 = document.getElementById('importantlinks');
+      text2 = document.getElementById('services');
+      text3 = document.getElementById('donations');
+    }
+    imgElement = document.querySelector('#flag');
+
+    if (textElement.innerHTML === englishText || textElement.innerHTML === englishTextE) {
+      if(additional)
+      {
+        textElement.innerHTML = polishText
+        text1.innerHTML = "Ważne odnośniki"
+        text2.innerHTML = "Moje usługi"
+        text3.innerHTML = "Darowizny na rozwój moich bezpłatnych kursów"
+      }
+      else
+      {
+        textElement.innerHTML = polishTextE
+      }
+    } else {
+      if(additional)
+      {
+        textElement.innerHTML = englishText
+        text1.innerHTML = "Important links"
+        text2.innerHTML = "My services"
+        text3.innerHTML = "Donations for the development of my free courses"
+      }
+      else
+      {
+        textElement.innerHTML = englishTextE
+      }
+    }
+    if (imgElement.src.includes(polishFlag)) {
+      imgElement.src = englishFlag;
+    } else {
+      imgElement.src = polishFlag;
+    }
+}
+
+
+
 /*!
  * This message applies to the code below.
  * Start Bootstrap - SB Admin 2 v4.1.3 (https://startbootstrap.com/theme/sb-admin-2)
