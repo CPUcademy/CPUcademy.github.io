a3cc4729feee35914d719e5444b29f600978df55
CPUcademy <cpucademy@gmail.com>
2024-10-23 21:22:50 +0200

    muscle up, it basics, rpi start

--- configs/script.js
+++ configs/script.js
@@ -2,9 +2,20 @@ const englishText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Qu
 const polishText = "Aliquam dictum ipsum in dui convallis hendrerit. Proin vitae egestas lectus, sed efficitur justo. Fusce et auctor nunc. Fusce dictum molestie auctor. In hac habitasse platea dictumst. Fusce ac mi ut dui tincidunt consequat accumsan non justo. Cras non tempus nisi. Ut laoreet tempus elit, sed ultricies neque dictum eget. Sed mollis convallis ipsum, porttitor sollicitudin nibh vulputate non. Nam ultricies tincidunt dolor, a finibus enim.";
 const englishTextE = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit nulla ullamcorper ante mollis, ac sagittis metus condimentum. Sed ex justo, mattis vel nulla a, euismod eleifend elit. Donec pellentesque velit ligula, at rutrum metus bibendum sit amet. Nullam lacus quam, aliquet et venenatis at, finibus sed dolor. Duis varius sed ipsum luctus sodales. Donec et odio vestibulum, lobortis risus in, rhoncus lectus. Quisque eu sodales sapien, et consequat ex. Fusce feugiat sapien ligula, sit amet varius tortor aliquet sed. Donec rhoncus ante nec nibh fringilla sodales. Aliquam dictum ipsum in dui convallis hendrerit. Proin vitae egestas lectus, sed efficitur justo. Fusce et auctor nunc. Fusce dictum molestie auctor. In hac habitasse platea dictumst. Fusce ac mi ut dui tincidunt consequat accumsan non justo. Cras non tempus nisi. Ut laoreet tempus elit, sed ultricies neque dictum eget. Sed mollis convallis ipsum, porttitor sollicitudin nibh vulputate non. Nam ultricies tincidunt dolor, a finibus enim. Vivamus consequat urna at magna elementum, vel vulputate turpis laoreet.";
 const polishTextE = "Aliquam dictum ipsum in dui convallis hendrerit. Proin vitae egestas lectus, sed efficitur justo. Fusce et auctor nunc. Fusce dictum molestie auctor. In hac habitasse platea dictumst. Fusce ac mi ut dui tincidunt consequat accumsan non justo. Cras non tempus nisi. Ut laoreet tempus elit, sed ultricies neque dictum eget. Sed mollis convallis ipsum, porttitor sollicitudin nibh vulputate non. Nam ultricies tincidunt dolor, a finibus enim. Vivamus consequat urna at magna elementum, vel vulputate turpis laoreet. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque suscipit nulla ullamcorper ante mollis, ac sagittis metus condimentum. Sed ex justo, mattis vel nulla a, euismod eleifend elit. Donec pellentesque velit ligula, at rutrum metus bibendum sit amet. Nullam lacus quam, aliquet et venenatis at, finibus sed dolor. Duis varius sed ipsum luctus sodales. Donec et odio vestibulum, lobortis risus in, rhoncus lectus. Quisque eu sodales sapien, et consequat ex. Fusce feugiat sapien ligula, sit amet varius tortor aliquet sed. Donec rhoncus ante nec nibh fringilla sodales.";
-
 const englishFlag = "files/englishFlag.png";
 const polishFlag = "files/polishFlag.png";
+
+const m1_p = "Muscle Up to aplikacja łącząca się przez Bluetooth z fizycznym urządzeniem, które wykorzystuje AI do analizy treningów przez kamerę w czasie rzeczywistym. Dane są później zwracane do aplikacji, w której urzytkownik ma dostęp do różnych statystyk, planów treningowych i wyzwań."
+const m2_p = "Pomysł na aplikację zrodził się z moich własnych doświadczeń. Podczas treningów, wielokrotnie zdarzało mi się rozpraszać liczeniem powtórzeń, co skutkowało gorszą formą podczas wykonywania ćwiczeń. Wymyśliłem, że stworzę coś, co nie tylko będzie liczyć za mnie powtórzenia, ale także sprawdzi, czy wykonuję je wystarczająco dobrze. Dodatkowo, kiedy wykona się odpowiednią ilość powtórzeń, urządzenie wydaje sygnał dźwiękowy, co całkowicie wyklucza zastanawianie się, czy osiągnęło się już swój cel."
+const m3_p = "Zacząłem pracę nad projektem w lutym 2023 roku, poznałem bibliotekę OpenCV i Mediapipe w języku Python i nauczyłem się języka Java z ukierunkowaniem na aplikacje w systemie Android. Zaprojektowałem obwód elektroniczny z mikrokomputerem Raspberry Pi, ekranem i innymi komponentami. Wszystko samodzielnie przylutowałem i włożyłem do specjalnie zmodyfikowanej obudowy. Później zabrałem się za samą aplikację, którą pisałem przez większość wakacji. Po zaprogramowaniu komunikacji Bluetooth z urządzeniem, zabrałem się za samo wykrywanie ćwiczeń. Program szuka punktów na ciele człowieka, na podstawie czego jest w stanie obliczyć kąty między tymi punktami, co pozwala na określenie stanu wykonania danego ćwiczenia. Jako że kamerę trzeba odpowiednio ustawić, przed rozpoczęciem sesji można transmitować podgląd na postawiony na RPi serwer lokalny. Sama aplikacja zawiera plany treningowe z opisami ćwiczeń i filmikami, różne statystki i wyzwania. Do urządzenia dołączona jest instrukcja obsługi."
+const m4_p = "Moja aplikacja może przydać się osobom, które łatwo się rozpraszają, np. z ADHD, ale nadal chcą być wysportowane. Oczywiście, aplikacji można używać też bez udziału urządzenia, co sprawia, że plany treningowe i wyzwania można realizować także poza domem. Podczas realizacji tego projektu, udało mi się zbudować aplikację mobilną od zera i połączyć jej funkcjonalności z modelem AI na mikrokomputerze. Jej wykonanie dużo mnie nauczyło, a wynik przerósł moje oczekiwania, ponieważ jest to mój pierwszy projekt na taką skalę. Jak zawsze, jest jeszcze miejsce na rozwój. Planuję przyłączyć wspomniany model AI prosto do aplikacji, co dałoby możliwość korzystania z jego funkcji wszędzie, nawet na zewnątrz, bez potrzeby posiadania dodatkowego urządzenia (i płacenia za nie). Aplikacja byłaby wtedy dostępna wszędzie dla wszystkich. Podczas tworzenia projektu, większości rzeczy uczyłem się na bieżąco. Taka umiejętność zdecydowanie przyda mi się w przyszłej pracy, więc cieszę się, że ją rozwinąłem."
+const m5_p = "Plakat projektu z konkursu Explory (2024/2025):"
+const m1_e = "Muscle Up is an app that connects via Bluetooth to a Raspberry Pi microcomputer. It utilizes AI to analyze workouts through a camera in real time. The data is then returned to the app, where the user can access various statistics, training plans, and challenges."
+const m2_e = "The idea for the app came from my own experiences. During workouts, I often got distracted by counting reps, which resulted in worse form during exercises. I came up with the idea of ​​creating something that would not only count reps for me but also check if I was doing them well enough. Additionally, when you do the right number of reps, the device beeps, which eliminates wondering if you have already achieved your goal."
+const m3_e = "I started working on the project in February 2023, got to know the OpenCV and Mediapipe libraries in Python, and learned Java with a focus on Android applications. I designed an electronic circuit with a Raspberry Pi microcomputer, a screen, and other components. I soldered everything myself and put it in a specially modified case. Then, I started working on the application, which I wrote for most of the holidays. After programming the Bluetooth communication with the device, I began to detect exercises. The program searches for points on the human body, based on which it can calculate the angles between these points, which allows you to determine the state of the exercise. Since the camera must be set up properly, before starting the session, you can broadcast a preview to a local server installed on the RPi. The application contains training plans with exercise descriptions and videos, various statistics, and challenges. The device comes with an instruction manual."
+const m4_e = "My app can be useful for people who are easily distracted, e.g., with ADHD, but still want to be fit. Of course, the app can also be used without the device, which means that training plans and challenges can also be completed outside the home. During this project, I managed to build a mobile app from scratch and combine its functionalities with an AI model on a microcomputer. Its implementation taught me a lot, and the result exceeded my expectations because it is my first project on such a scale. As always, there is still room for improvement. I plan to connect the AI above model directly to the app, which would make it possible to use its functions everywhere, even outdoors, without the need for an additional device (and paying for it). The app would then be available everywhere for everyone. While creating the project, I learned most things on the fly. This skill will be useful in my future work, so I am glad I developed it."
+const m5_e = "Explory competition project poster (2024/2025):"
+
 if(window.location.pathname.endsWith('index.html') || (window.location.pathname.endsWith('cv.html')))
 {
   textElement = document.getElementById('cv');
@@ -51,6 +62,44 @@ if(window.location.pathname.endsWith('index.html') || (window.location.pathname.
         {
           textElement.innerHTML = englishTextE
         }
+      }
+      if (imgElement.src.includes(polishFlag)) {
+        imgElement.src = englishFlag;
+      } else {
+        imgElement.src = polishFlag;
+      }
+  }
+}
+
+if(window.location.pathname.endsWith('muscleup.html'))
+{
+  m1 = document.getElementById('m1');
+  m2 = document.getElementById('m2');
+  m3 = document.getElementById('m3');
+  m4 = document.getElementById('m4');
+  m5 = document.getElementById('m5');
+  m1.innerHTML = m1_e
+  m2.innerHTML = m2_e
+  m3.innerHTML = m3_e
+  m4.innerHTML = m4_e
+  m5.innerHTML = m5_e
+
+  function languageM() 
+  {
+      imgElement = document.querySelector('#flag');
+
+      if (m1.innerHTML == m1_e) {
+          m1.innerHTML = m1_p
+          m2.innerHTML = m2_p
+          m3.innerHTML = m3_p
+          m4.innerHTML = m4_p
+          m5.innerHTML = m5_p
+      } else {
+        m1.innerHTML = m1_e
+        m2.innerHTML = m2_e
+        m3.innerHTML = m3_e
+        m4.innerHTML = m4_e
+        m5.innerHTML = m5_e
       }
       if (imgElement.src.includes(polishFlag)) {
         imgElement.src = englishFlag;
